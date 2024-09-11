d0b883ce7bb7286948408da7a99a7ff2d5b0996e
KartikPat250905 <kartikpat25@gmail.com>
2024-09-11 21:19:55 +0300

    changed datetype from int to float

--- 6/6.3.py
+++ 6/6.3.py
@@ -2,7 +2,7 @@ def convert(gallons):
     return gallons * 3.785
 
 while True:
-    gallons = int(input("Enter the number of gallons or -1 to quit"))
+    gallons = float(input("Enter the number of gallons or -1 to quit"))
     if gallons < 0:
         break
     print(f"{gallons} gallons are {convert(gallons)}")
